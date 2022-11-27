bf9adae9d98aba53d7ce551a038c251adb5a26d9
Petros <psdeme@hotmail.com>
2022-11-27 15:24:39 +0100

    removed unused optimized_score_function

--- competitive_sudoku/team09_A1/sudokuai.py
+++ competitive_sudoku/team09_A1/sudokuai.py
@@ -62,46 +62,6 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
             illegal = get_filled_row_values(row_index, state) + get_filled_column_values(
                 col_index, state) + get_filled_region_values(row_index, col_index, state)
             return set(illegal)  # easy way to remove duplicates
-        
-        def optimized_score_function(move: Move, state: GameState):
-            # return the score that should be added to a player after the given move
-            # if the player has no score increase after the move, this function returns 0
-            row = get_filled_row_values(move.i, state)
-            col = get_filled_column_values(move.j, state)
-            block = get_filled_region_values(move.i, move.j, state)
-            # N, excluding the cell under question (the cell we consider fo filling in)
-            # we exclude it because scoreing function is called BEFORE the cell gets filled
-            full_len = state.board.N - 1
-
-            # based onn the logic mentioned in the Assignment desctiption, we calculate score increase after the move
-            # case where a row, a column and a block are completed after the move
-            if len(row) == full_len and len(col) == full_len and len(block) == full_len:
-                return 7
-            # case where a row and a column is completed
-            elif len(row) == full_len and len(col) == full_len:
-                return 3
-            # case where a row and a block is completed
-            elif len(row) == full_len and len(block) == full_len:
-                return 3
-            # case where a col and a block is completed
-            elif len(col) == full_len and len(block) == full_len:
-                return 3
-            # case where only 1 among column, row and block are completed
-            elif len(row) == full_len or len(col) == full_len or len(block) == full_len:
-                return 1
-            elif len(row) == full_len - 1 or len(col) == full_len - 1 or len(block) == full_len - 1:
-                row_about_to_complete = len(row) == full_len - 1
-                col_about_to_complete = len(col) == full_len - 1
-                block_about_to_complete = len(block) == full_len - 1
-                sum_complete = row_about_to_complete + col_about_to_complete + block_about_to_complete
-                if sum_complete == 1:
-                    return -2
-                elif sum_complete == 2:
-                    return -4
-                elif sum_complete == 3:
-                    return -8
-            else:
-                return 0
 
         def score_function(move: Move, state: GameState):
             # return the score that should be added to a player after the given legal_move
@@ -274,6 +234,4 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         if best_move is not None:
             self.propose_move(best_move)
 
-# DERIVE A FORMULA PROVIDING DEPTH INVERSELY PROPORTIONATE TO AVAILABLE LEGAL MOVES
-# THIS IS BECAUSE WHEN WE HAVE MORE LEGAL MOVES IT TAKES MORE TIME TO REACH A LEAF STATE
-# THUS WE LIMIT THE DEPTH MORE IN THE BEGINING OF THE GAME (MORE LEGAL MOVES AVAILABLE AND DEEPER TREE)
+
